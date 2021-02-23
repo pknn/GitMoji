1f8f945853534a702523f0cbda84facc7d4f985b
Pakanon Pantisawat <pakanon.pk@gmail.com>
2021-02-23 15:40:03 +0700

    Initial Commit

---
+++ GitMoji/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  GitMoji
+//
+//  Created by Pakanon Pantisawat on 23/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
